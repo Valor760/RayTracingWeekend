ee048a0c3e2f9833698b20de310966c49a4362c0
Valor760 <31585840+Valor760@users.noreply.github.com>
2022-04-12 16:31:37 +0300

    Done without Defocus Blur

--- src/camera.h
+++ src/camera.h
@@ -7,20 +7,30 @@
 
 class camera {
     public:
-        camera() {
+        camera(
+            point3 lookfrom,
+            point3 lookat,
+            vec3 vup,
+            double v_fov, 
+            double aspect_ratio
+        ) {
             // Camera
-            auto aspect_ratio = 16.0 / 9.0;
-            float viewport_height = 2.0;
+            auto theta = deg_to_rad(v_fov);
+            auto h = tan(theta / 2);
+            float viewport_height = 2.0 * h;
             float viewport_width = aspect_ratio * viewport_height;
-            float focal_length = 1.0;
 
-            origin = point3(0, 0, 0);
-            horizontal = vec3(viewport_width, 0, 0);
-            vertical = vec3(0, viewport_height, 0);
-            lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+            auto w = unit_vector(lookfrom - lookat);
+            auto u = unit_vector(cross(vup, w));
+            auto v = cross(w, u);
+
+            origin = lookfrom;
+            horizontal = viewport_width * u;
+            vertical = viewport_height * v;
+            lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
         }
 
-        ray get_ray(double u, double v) {
+        ray get_ray(double u, double v) const {
             return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
         }
 
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,51 @@
 #include "camera.h"
 #include "material.h"
 
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<diffuse>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<diffuse>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<glass>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<glass>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<diffuse>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
 
 color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
@@ -34,27 +79,29 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 int main(int argc, char** argv) {
     // Image
     constexpr auto aspect_ratio = 16.0 / 9.0;
-    constexpr int image_width = 1600;
+    constexpr int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     constexpr int samples_per_pixel = 100;
     constexpr int max_depth = 10;
 
     // World
-    hittable_list world;
+    hittable_list world = random_scene();
 
-    auto material_ground = make_shared<diffuse>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<diffuse>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    // auto material_ground = make_shared<diffuse>(color(0.8, 0.8, 0.0));
+    // auto material_center = make_shared<glass>(1.5);
+    // auto material_left   = make_shared<glass>(1.5);
+    // auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    // world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    // world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
-    camera cam;
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    camera cam(lookfrom, lookat, vec3(0,1,0), 45, aspect_ratio);
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
